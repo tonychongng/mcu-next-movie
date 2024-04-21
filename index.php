0f22dc60df61877483c8297311043b72d4b6a58e
tony123x <tonychongng123@gmail.com>
2024-04-22 00:14:46 +0200

    add untilMessage

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <?php
+
+require 'functions.php';
 const API_URL = "https://whenisthenextmcufilm.com/api";
 
-$curl = curl_init();
+$data = get_data(API_URL);
+$untilMessage = get_until_message($data["days_until"]);
 
-curl_setopt_array($curl , array(
-        CURLOPT_URL => API_URL,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_SSL_VERIFYPEER => false,
-));
-
-$response = curl_exec($curl);
-
-$data = json_decode($response, true);
-
-$err = curl_error($curl);
-if($err) {
-    var_dump($err);
-}else {
-    curl_close($curl);
-}
 ?>
 
 <head>
@@ -34,7 +20,7 @@ if($err) {
         <h1>Próxima película</h1>
         <img src="<?= $data["poster_url"] ?>" width="300" poster de la próxima película"/>
 
-        <p><?= $data["title"] ?> se estrena en <?= $data["days_until"] ?> días</p>
+        <p><?= $data["title"];?> - <?= $untilMessage ?></p>
 
         <p>Se estrena el: <?= $data["release_date"] ?></p>
     </section>
@@ -55,8 +41,8 @@ if($err) {
     }
 
     body {
-        display: grid;
-        place-content: center;
+        display: flex;
+        justify-content: center;
     }
 
     section {
